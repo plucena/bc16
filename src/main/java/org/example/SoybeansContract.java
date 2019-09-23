d57a1520671ef1728f4647a15b1b4caf6015e9b4
Percival Silva de Lucena <plucena@br.ibm.com>
2019-09-23 14:49:56 -0300

    V1

---
+++ src/main/java/org/example/SoybeansContract.java
@@ -0,0 +1,80 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.example;
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.contract.annotation.Contact;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.License;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Contract(name = "SoybeansContract",
+    info = @Info(title = "Soybeans contract",
+                description = "My Smart Contract",
+                version = "0.0.1",
+                license =
+                        @License(name = "Apache-2.0",
+                                url = ""),
+                                contact =  @Contact(email = "bc16@example.com",
+                                                name = "bc16",
+                                                url = "http://bc16.me")))
+@Default
+public class SoybeansContract implements ContractInterface {
+    public  SoybeansContract() {
+
+    }
+    @Transaction()
+    public boolean soybeansExists(Context ctx, String soybeansId) {
+        byte[] buffer = ctx.getStub().getState(soybeansId);
+        return (buffer != null && buffer.length > 0);
+    }
+
+    @Transaction()
+    public void createSoybeans(Context ctx, String soybeansId, String value) {
+        boolean exists = soybeansExists(ctx,soybeansId);
+        if (exists) {
+            throw new RuntimeException("The asset "+soybeansId+" already exists");
+        }
+        Soybeans asset = new Soybeans();
+        asset.setValue(value);
+        ctx.getStub().putState(soybeansId, asset.toJSONString().getBytes(UTF_8));
+    }
+
+    @Transaction()
+    public Soybeans readSoybeans(Context ctx, String soybeansId) {
+        boolean exists = soybeansExists(ctx,soybeansId);
+        if (!exists) {
+            throw new RuntimeException("The asset "+soybeansId+" does not exist");
+        }
+
+        Soybeans newAsset = Soybeans.fromJSONString(new String(ctx.getStub().getState(soybeansId),UTF_8));
+        return newAsset;
+    }
+
+    @Transaction()
+    public void updateSoybeans(Context ctx, String soybeansId, String newValue) {
+        boolean exists = soybeansExists(ctx,soybeansId);
+        if (!exists) {
+            throw new RuntimeException("The asset "+soybeansId+" does not exist");
+        }
+        Soybeans asset = new Soybeans();
+        asset.setValue(newValue);
+
+        ctx.getStub().putState(soybeansId, asset.toJSONString().getBytes(UTF_8));
+    }
+
+    @Transaction()
+    public void deleteSoybeans(Context ctx, String soybeansId) {
+        boolean exists = soybeansExists(ctx,soybeansId);
+        if (!exists) {
+            throw new RuntimeException("The asset "+soybeansId+" does not exist");
+        }
+        ctx.getStub().delState(soybeansId);
+    }
+
+}
---
+++ src/test/java/org/example/SoybeansContractTest.java
@@ -0,0 +1,174 @@
+/*
+ * SPDX-License-Identifier: Apache License 2.0
+ */
+
+package org.example;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.nio.charset.StandardCharsets;
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+
+public final class SoybeansContractTest {
+
+    @Nested
+    class AssetExists {
+        @Test
+        public void noProperAsset() {
+
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            when(stub.getState("10001")).thenReturn(new byte[] {});
+            boolean result = contract.soybeansExists(ctx,"10001");
+
+            assertFalse(result);
+        }
+
+        @Test
+        public void assetExists() {
+
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            when(stub.getState("10001")).thenReturn(new byte[] {42});
+            boolean result = contract.soybeansExists(ctx,"10001");
+
+            assertTrue(result);
+
+        }
+
+        @Test
+        public void noKey() {
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            when(stub.getState("10002")).thenReturn(null);
+            boolean result = contract.soybeansExists(ctx,"10002");
+
+            assertFalse(result);
+
+        }
+
+    }
+
+    @Nested
+    class AssetCreates {
+
+        @Test
+        public void newAssetCreate() {
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            String json = "{\"value\":\"TheSoybeans\"}";
+
+            contract.createSoybeans(ctx, "10001", "TheSoybeans");
+
+            verify(stub).putState("10001", json.getBytes(UTF_8));
+        }
+
+        @Test
+        public void alreadyExists() {
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            when(stub.getState("10002")).thenReturn(new byte[] { 42 });
+
+            Exception thrown = assertThrows(RuntimeException.class, () -> {
+                contract.createSoybeans(ctx, "10002", "TheSoybeans");
+            });
+
+            assertEquals(thrown.getMessage(), "The asset 10002 already exists");
+
+        }
+
+    }
+
+    @Test
+    public void assetRead() {
+        SoybeansContract contract = new  SoybeansContract();
+        Context ctx = mock(Context.class);
+        ChaincodeStub stub = mock(ChaincodeStub.class);
+        when(ctx.getStub()).thenReturn(stub);
+
+        Soybeans asset = new  Soybeans();
+        asset.setValue("Valuable");
+
+        String json = asset.toJSONString();
+        when(stub.getState("10001")).thenReturn(json.getBytes(StandardCharsets.UTF_8));
+
+        Soybeans returnedAsset = contract.readSoybeans(ctx, "10001");
+        assertEquals(returnedAsset.getValue(), asset.getValue());
+    }
+
+    @Nested
+    class AssetUpdates {
+        @Test
+        public void updateExisting() {
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+            when(stub.getState("10001")).thenReturn(new byte[] { 42 });
+
+            contract.updateSoybeans(ctx, "10001", "updates");
+
+            String json = "{\"value\":\"updates\"}";
+            verify(stub).putState("10001", json.getBytes(UTF_8));
+        }
+
+        @Test
+        public void updateMissing() {
+            SoybeansContract contract = new  SoybeansContract();
+            Context ctx = mock(Context.class);
+            ChaincodeStub stub = mock(ChaincodeStub.class);
+            when(ctx.getStub()).thenReturn(stub);
+
+            when(stub.getState("10001")).thenReturn(null);
+
+            Exception thrown = assertThrows(RuntimeException.class, () -> {
+                contract.updateSoybeans(ctx, "10001", "TheSoybeans");
+            });
+
+            assertEquals(thrown.getMessage(), "The asset 10001 does not exist");
+        }
+
+    }
+
+    @Test
+    public void assetDelete() {
+        SoybeansContract contract = new  SoybeansContract();
+        Context ctx = mock(Context.class);
+        ChaincodeStub stub = mock(ChaincodeStub.class);
+        when(ctx.getStub()).thenReturn(stub);
+        when(stub.getState("10001")).thenReturn(null);
+
+        Exception thrown = assertThrows(RuntimeException.class, () -> {
+            contract.deleteSoybeans(ctx, "10001");
+        });
+
+        assertEquals(thrown.getMessage(), "The asset 10001 does not exist");
+    }
+
+}
